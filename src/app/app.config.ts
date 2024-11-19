180c8eb5fcdee13c82f09198d7b66b200fd0276e
Nata <melvia.nr@gmail.com>
2024-11-19 22:54:19 +0500

    feat: create routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, Routes} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+
+// определение маршрутов
+const appRoutes: Routes =[
+  { path: "", component: HomeComponent},
+  { path: "about", component: AboutComponent},
+  { path: "**", component: NotFoundComponent }
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes)]
 };
+
